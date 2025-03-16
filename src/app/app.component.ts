a2fbdc1b408e9c9368eb5975f4874443336d32f3
atul275 <kesharwaniatul275@gmail.com>
2025-03-16 18:08:57 +0530

    Pass data using router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-Routing';
+  showLoader: boolean = false;
+  router: Router = inject(Router);
+
+  ngOnInit() {
+    this.router.events.subscribe((routerEvent) => {
+      if (routerEvent instanceof NavigationStart) {
+        this.showLoader = true;
+      }
+
+      if (routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationCancel || routerEvent instanceof NavigationError) {
+        this.showLoader = false;
+      }
+    })
+  }
 }
--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Course } from '../Models/course';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit{
+  activeRoute: ActivatedRoute= inject(ActivatedRoute);
+  router: Router= inject(Router);
+  course: any;
+
+  ngOnInit() {
+    // this.activeRoute.data.subscribe((data)=> {
+    //   this.course= data;
+    // })
+    // this.course= this.router.getCurrentNavigation()?.extras.state;
+    this.course= history.state;
+  }
 
 }
